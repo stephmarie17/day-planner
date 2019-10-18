7d28b7b324f301d07430aeae97fcb3337ac62c15
Stephanie Aurelio <stephanie.m.aurelio@gmail.com>
2019-10-17 18:21:15 -0700

    updated the hours to moment objects

--- script.js
+++ script.js
@@ -14,28 +14,31 @@ let currentHour = m.format("hA");
 
 console.log(currentHour);
 
-
-
-// function updateTimeblock() {
-// // Toggle class to past
-//     if(moment().isBefore($("#hourBlock").text)) {
-//         $("#hour").toggleclass("future");
-// // Toggle class to present
-//     } else if(moment().isSame($("#hourBlock").text)) {
-//         $("#hour").toggleclass("present");
-// // Toggle class to future
-//     } else if(moment().isAfter($("#hourBlock").text)) {
-//         $("#hour").toggleclass("past");
-//     }
-// };
-
-// updateTimeblock();
-
+var nine = m.hour(9).format("hA");
+$("#slotOne").text(nine);
+var ten = m.hour(10).format("hA");
+$("#slotTwo").text(ten);
+var eleven = m.hour(11).format("hA");
+$("#slotThree").text(eleven);
+var twelve = m.hour(12).format("hA");
+$("#slotFour").text(twelve);
+var one = m.hour(13).format("hA");
+$("#slotFive").text(one);
+var two = m.hour(14).format("hA");
+$("#slotSix").text(two);
+var three = m.hour(15).format("hA");
+$("#slotSeven").text(three);
+var four = m.hour(16).format("hA");
+$("#slotEight").text(four);
+var five = m.hour(17).format("hA");
+$("#slotNine").text(five);
 
 // Problem 2: user input field
 
 // Each time block will have a text area where the user can enter text
+
 // Don't forget event.preventdefault()
 
 // Problem 3: save button
+
 // When a user enters info into input field and clicks the save button, this triggers storing the time and input in localStorage
